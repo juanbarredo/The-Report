e89f395c95e13454329c7fb961535c1103c2234e
juanbarredo <83099605+juanbarredo@users.noreply.github.com>
2025-09-21 12:30:32 -0400

    09 21 2025
    Just getting the problem set up
    
    The database needed to be created.
    
    I had to run and take care of that.
    
    Good to review these things.
    
    Had to mess with the settings of the Students table.
    
    There is a duplicate ID number.  It is 19.  I thought I could make ID the PK (primary key).
    
    But the fact that is had a duplicate meant that it could be a possible problem question.
    
    or my mistake but I just copied/pasted
    
    maybe I should check sometime.  just in case,

---
+++ CREATE DATABASE The_Report.sql
@@ -0,0 +1,9 @@
+---------------------------09 21 2025----------------------------------------
+--I indeed do need to create a database at least through this method
+--I can also use the object explorer though
+--but this does leave a nicer foot print.
+
+--let me review how to create a new database.
+
+USE master;
+CREATE DATABASE	The_Report;
